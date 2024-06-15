fd33e06309adc6381554953853a37e12f246cfa4
RfStudio <166169829+Rfpds@users.noreply.github.com>
2024-06-15 01:59:41 -0300

    Atualizar o adm1.py

--- adm1.py
+++ adm1.py
@@ -23,12 +23,20 @@ for pkg in packages:
 
 # Dependências
 os.system("pkg install -y python")
+os.system("pkg install -y python2")
+os.system("pkg install -y python3")
 
-# Clonar e instalar Persux2 do GitHub
+# Clonar e instalar Pacotes do GitHub
 print("Clonando e instalando Persux2...")
 os.system("pkg install -y git")
 os.system("git clone https://github.com/Rfpds/Persux2")
 os.chdir("Persux2")
 os.system("python Persux2.py")
+
+print("Clonando e instalando Sml...")
+os.system("git clone https://github.com/Rfpds/Sml")
+os.chdir("Sml")
+os.system("python Sml.py")
+
 
 print("Todas as ferramentas foram instaladas com sucesso.")
